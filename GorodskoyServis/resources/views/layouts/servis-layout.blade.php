ef14164545130c0b1042455f71e3cd8bc27e49d7
Vasiley-ish <vacileypds@mail.ru>
2021-12-13 11:03:16 +0400

    начал верстать первую страницу

--- GorodskoyServis/resources/views/layouts/servis-layout.blade.php
+++ GorodskoyServis/resources/views/layouts/servis-layout.blade.php
@@ -4,18 +4,39 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="..\resources\css\app.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="..\..\css\app.css">
+    <title>@yield('title')</title>
 </head>
 
 <body>
-    <header></header>
+    
+<!-- ---------------------------------- -->
+<header>
+        <div class="header-content">
+
+        <img src="{{ URL::to('/assets/images/logo.png') }}">
+
+        <nav>
+            <a href="#">Главная</a>
+            <a href="#">Регистрация</a>
+            <a href="#">Вход</a>
+        </nav>
+
+        </div>
+    </header>
+<!-- ---------------------------------- -->
+    <main>
+        <div class="content">
+            @yield('content')
+        </div>
+    </main>
 
 
-@include('content')
+<!-- ---------------------------------- -->
 
+<footer></footer>
 
-    <footer></footer>
+<!-- ---------------------------------- -->
 </body>
 
 </html>
